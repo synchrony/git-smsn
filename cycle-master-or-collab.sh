729b8f84cb3cda8cf1642a4594ec2a15f06f4e29
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-03-04 01:12:26 -0800

    cycle*.sh: announce (echo to console upon starting) each stage

--- cycle-master-or-collab.sh
+++ cycle-master-or-collab.sh
@@ -4,9 +4,14 @@ read -p "Did you export the graph to vcs? " -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  bash add.sh\
+  bash
+    printf    "\n\n Beginning add.sh.\n"
+    && add.sh\
+    && printf "\n\nBeginning commit.sh.\n"
     && bash commit.sh\
+    && printf "\n\nBeginning pull*.sh.\n"
     && bash pull-master-or-collab.sh\
+    && printf "\n\nBeginning push*.sh.\n"
     && bash push-master-or-collab.sh\
     && mv ../it ../it.$(date +"%Y_%m_%d_%I_%M_%p")
 fi
