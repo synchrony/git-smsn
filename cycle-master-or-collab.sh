b663dbd04966a5dfb449fff21a94510b4cca8e43
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-03-04 02:00:05 -0800

    fix a typo: printf does not need a leading bash command

--- cycle-master-or-collab.sh
+++ cycle-master-or-collab.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-read -p "Did you export the graph to vcs? " -n 1 -r
-echo ""
+echo "Did you export the graph to the VCS folders here?"
+read -p "Did you then run lost-nodes.sh to verify nothing disappeared?" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  bash
-    printf    "\n\n Beginning add.sh.\n"
-    && add.sh\
-    && printf "\n\nBeginning commit.sh.\n"
+  printf "\n\nBeginning add.sh.\n"\
+    && bash add.sh\
+    && printf "\n\nBeginning commit.sh.\n"\
     && bash commit.sh\
-    && printf "\n\nBeginning pull*.sh.\n"
+    && printf "\n\nBeginning pull*.sh.\n"\
     && bash pull-master-or-collab.sh\
-    && printf "\n\nBeginning push*.sh.\n"
+    && printf "\n\nBeginning push*.sh.\n"\
     && bash push-master-or-collab.sh\
     && mv ../it ../it.$(date +"%Y_%m_%d_%I_%M_%p")
 fi
