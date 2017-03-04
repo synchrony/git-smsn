24b4952e30bf34cca6885324360f36d3f1e29be3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-03-04 01:59:00 -0800

    clarify console reports

--- lost-nodes.sh
+++ lost-nodes.sh
@@ -5,13 +5,12 @@ ADDS=temp.new-files # to collect the added files from the 4 repos
 echo "" > $DELS
 echo "" > $ADDS
 for DIR in private personal public universal; do
-  echo "changing to $DIR"
   cd $DIR
   git ls-files -d >> ../$DELS
   git ls-files -o >> ../$ADDS
-  cd -
+  cd ..
 done
 sort $DELS > $DELS.sorted
 sort $ADDS > $ADDS.sorted
 comm $DELS.sorted $ADDS.sorted
-echo "If you see anything in the first column, that's a problem."
+echo "If you see any node addresses in the first column above, then something disappeared."
