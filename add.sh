a01712511bf165d90947be25f032666c429b945e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-08-11 20:13:58 -0700

    escape the dot

--- add.sh
+++ add.sh
@@ -3,7 +3,7 @@
 for dir in private personal public universal; do
   cd ${dir}
   echo "adding new atoms in "${dir}
-  git ls-files --others | grep ".smsn$" | xargs git add
+  git ls-files --others | grep "\.smsn$" | xargs git add
   git add -A
   cd -
 done
