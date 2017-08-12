a9b725926be14ab75d3316a4e0f7860986c2a046
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-08-11 17:54:32 -0700

    add a script for renaming the old neo4j file to neo4j.the-current-time

---
+++ if-the-4-branch-names-vary/fresh-clones.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone -b collab file:///home/jeff/work/git_jbb/smsn-private.git
+git clone -b collab file:///home/jeff/work/git_jbb/smsn-personal.git
+git clone https://github.com/synchrony/data-public.git
+git clone https://github.com/synchrony/data-universal.git
+
+mv smsn-private private
+mv smsn-personal personal
+mv data-public public
+mv data-universal universal
