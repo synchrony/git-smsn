4a5e2f54d187df22bfc0a26910f76be62690536c
Joshua Shinavier <josh@fortytwo.net>
2018-02-26 22:06:21 -0800

    Minor tweaks

--- add.sh
+++ add.sh
@@ -2,8 +2,7 @@
 
 for dir in `cat list-of-repositories.txt`; do
   cd ${dir}
-  echo "adding new atoms in "${dir}
-  git ls-files --others | grep "\.smsn$" | xargs git add
-  git add -A
+  echo "adding new atoms in ${dir}"
+  git ls-files --others | grep ".smsn$" | xargs git add
   cd -
 done
