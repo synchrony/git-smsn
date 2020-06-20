3711ed6f31678d229af88864b5fe663b30935954
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-06-20 12:56:57 -0500

    change permissions change observatorio repo location

--- if-the-4-branch-names-vary/fresh-clones.sh
+++ if-the-4-branch-names-vary/fresh-clones.sh
@@ -4,12 +4,12 @@ git clone file:///home/jeff/work/git_jbb/kb-extras.git
 git clone -b collab file:///home/jeff/work/git_jbb/smsn-private.git
 git clone -b collab file:///home/jeff/work/git_jbb/smsn-personal.git
 git clone https://github.com/synchrony/data-public.git
-git clone https://github.com/synchrony/observatorio-fiscal-smsn.git
 git clone https://github.com/synchrony/data-universal.git
+git clone https://github.com/JeffreyBenjaminBrown/observatorio-fiscal-smsn.git
 
 mv kb-extras metadata
 mv smsn-private private
 mv smsn-personal personal
 mv data-public public
-mv observatorio-fiscal-smsn observatorio
 mv data-universal universal
+mv observatorio-fiscal-smsn observatorio
